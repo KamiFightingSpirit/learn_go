98855692d1c23ee71c37dd3d58b64e287a7c214a
Andrew DeHuff <a.dehuff4@gmail.com>
2024-08-23 17:19:06 -0700

    Learning Data Types

--- main.go
+++ main.go
@@ -13,4 +13,37 @@ func main() {
 
 	banana_res := banana.AddBananas(4, 5, "+")
 	fmt.Println(banana_res)
+
+	banana_res2 := banana.MinusBanana(4, 5, "-")
+	fmt.Println(banana_res2)
+
+	banana_var_import := banana.BananaScopedVar
+	fmt.Println(banana_var_import)
+
+	var a, b, c = 1, 2, "Coders"
+	fmt.Println(a, b, c)
+
+	//block scoping
+	{
+		var blockVariable int = 5
+		fmt.Println(blockVariable)
+	}
+
+	//out of scope here
+	// fmt.Println(blockVariable)
+
+	//arrays
+	var intArray [3]int = [3]int{1, 2, 3}
+	fmt.Println(intArray)
+
+	//pointers in Go
+	var originalVar int = 42
+	var pointerVar = &originalVar
+
+	originalVar = 43
+	fmt.Println(originalVar, *pointerVar, pointerVar)
+
+	//structs in Go
+	var Person = banana.Person{Name: "Andrew", Age: 37, Location: "San Francisco, CA"}
+	fmt.Println(Person)
 }
--- types/banana.go
+++ types/banana.go
@@ -1,5 +1,13 @@
 package banana
 
+var BananaScopedVar = "I am a variable that is hopefully only scoped to banana package"
+
+type Person struct {
+	Name     string
+	Age      int
+	Location string
+}
+
 func AddBananas(a int, b int, arg string) int {
 	var sum int
 
@@ -8,4 +16,14 @@ func AddBananas(a int, b int, arg string) int {
 	}
 
 	return sum
+}
+
+func MinusBanana(a int, b int, arg string) int {
+	var result int
+
+	if arg == "-" {
+		result = a - b
+	}
+
+	return result
 }
